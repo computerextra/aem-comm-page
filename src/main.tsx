84577f1d8294c328a9640a338412982d97644085
jayzone91 <register@j4yz0n3.de>
2024-07-05 09:27:58 +0200

    Added Tailwindcss

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Home from "./pages/Home";
+import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<>Home</>} />
+        <Route path="/" element={<Home />} />
       </Routes>
     </BrowserRouter>
   </React.StrictMode>,
---
+++ src/pages/Home.tsx
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <div>
+      <h1 className="text-3xl bg-green-600 font-bold underline">Test</h1>
+    </div>
+  );
+}
