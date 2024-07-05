3a4f7f7ac278baeb69038207a6d99563d8c9780d
jayzone91 <register@j4yz0n3.de>
2024-07-05 09:41:05 +0200

    chenged parameters

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,6 @@ import react from "@vitejs/plugin-react-swc";
 // https://vitejs.dev/config/
 export default defineConfig({
   // Enable for Github Pages!
-  base: "/",
+  base: "./",
   plugins: [react()],
 });
